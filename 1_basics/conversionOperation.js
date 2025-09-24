0fcad4244885f38e8659d5513037549f27ec552d
KRWT10 <kartikrwt2724@gmail.com>
2025-09-24 05:56:32 +0000

    conversion of datatypes

---
+++ 1_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+// let play = 89
+// console.log(typeof play);
+// console.log(typeof (play));
+
+
+// let valueInNumber = Number(play)
+// console.log(play)
+
+// "89" => 89
+// "89hsi" => NaN
+// True => 1 , False => 0
+
+
+let isLoggedin = ""
+
+let booleanIsLoggedIn = Boolean( isLoggedin)
+//console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"kartik" =>true
+
+let count = "47"
+let stringCount = String(count)
+console.log(stringCount);
+console.log(typeof stringCount);
+

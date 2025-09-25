05cc8935c41a2341641f817cfb77591f546222c6
Kartik Rawat <kartikrwt2724@gmail.com>
2025-09-25 06:33:12 +0000

    datatypes

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"USE STRICT" // treat all JS code as newer version
+//alert(" 3 + 3 ") // we are using node JS not browser 
+
+let Name= "kartik"
+let age= 20
+let isLoggedin= false
+
+//types of data types
+
+// number
+// string
+// boolean 
+// bigint => for very big values
+// Null => standalone value(empty)
+// undefined = 
+// symbol => for unique  
+
+
+// object 
+
+console.log(typeof undefined)// undefined 
+console.log(typeof null)  // object

e7e1ee4aedf62bf6129ac558b763dc66dfbee153
Kartik Rawat <kartikrwt2724@gmail.com>
2025-09-25 06:33:38 +0000

    comparisons

---
+++ 1_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+
+console.log(null >0)
+console.log(null <0)
+console.log(null >=0);
+console.log(null <=0);
+
+
+console.log(null == undefined)
+
+console.log(undefined >0)
+console.log(undefined <0)
+console.log(undefined >=0)
+console.log(undefined <=0)
+console.log(undefined == null)
+ 
+
+console.log(2 != 1) ;
+
+// always code clean and readable rakho
+

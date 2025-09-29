b3a6ae2a19fc6fd6b669268d1cb411b4c0318998
Kartik Rawat <kartikrwt2724@gmail.com>
2025-09-29 06:24:28 +0000

    Arrays part 1

---
+++ 2_basics /01_Arrays.js
@@ -0,0 +1,31 @@
+const myArr1 =[1, 2 , 3, 4, 5]
+//console.log (myArr1[3])  
+
+const myArr2 = new Array(7, 8, 9, 6, 5, 2)
+//
+
+
+// array methods 
+
+// myArr1.push(4)
+// myArr1.pop()
+// console.log(myArr1)
+
+// myArr1.unshift(10)
+// myArr1.shift()
+
+
+
+
+// console.log(newArr)
+// console.log(typeof newArr)   
+
+
+console.log("A", myArr1);
+const myn1 = myArr1.slice(1, 3)
+
+console.log(myn1);
+
+console.log("B", myArr1);
+const myn2 = myArr1.splice(1, 3)
+console.log(myn2)

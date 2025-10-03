11687328824fe29cff0873b9e54a2681e467ce7d
Kartik Rawat <kartikrwt2724@gmail.com>
2025-10-03 05:03:39 +0000

    functions

--- 3_basics/01_functions.js
+++ 3_basics/01_functions.js
@@ -19,10 +19,38 @@ const result = addTwoNumbers(99, 138)
 
 function newUserLoggedInMessage(username){
     if (!username) {
-        console.log("Username is required");
-        return "Please enter your username"
+        // console.log("Username is required");
+        // return "Please enter your username"
     }
-    return `Welcome back ${username} , we missed you!`
+    // return `Welcome back ${username} , we missed you!`
 }
-const message = newUserLoggedInMessage("")
-console.log(message);
+// const message = newUserLoggedInMessage("")
+// console.log(message);
+
+
+function calculateCartprice(...Num1) {
+    return Num1
+}
+// console.log(calculateCartprice(200 , 711 ,520 ,622 ,122 ));
+
+
+const object ={
+    name: "kartik",
+    email: "k@online.com"
+}
+function handlingObj(allobject) {
+    console.log(`the name is ${allobject.name}, the email is ${allobject.email}`);
+}
+handlingObj(object)
+handlingObj(
+    {
+        name : "tu chup rehe",
+        email : "tu@gmail.com"
+    }
+)
+
+const myNewArray =[200, 300,600, ]
+function returnValue(allArray) {
+    return allArray[2]
+}
+console.log(returnValue([300,699,400]));

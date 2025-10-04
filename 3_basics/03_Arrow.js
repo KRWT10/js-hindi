bff6947ee4d9ad13cbfedf4f621549e4b91708fb
Kartik Rawat <kartikrwt2724@gmail.com>
2025-10-04 05:18:32 +0000

    arrows

--- 3_basics/03_Arrow.js
+++ 3_basics/03_Arrow.js
@@ -9,3 +9,17 @@ const user = {
 user.greet()
 console.log(this);
 
+
+
+() => {}    // this is arrow function 
+
+const add3 = (num1 , num2 )=> {
+  return  num1 + num2 
+}
+console.log(add3(8 , 8))
+
+
+
+// const add4 = (num1 , num2 )=> num1 + num2
+const add4 = (num1 , num2 )=> ({username : "kartik rawat "})
+console.log(add4(10 , 8))

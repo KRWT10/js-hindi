d5c76f8494bafdd09707a37382b6725d96f0839f
Kartik Rawat <kartikrwt2724@gmail.com>
2025-10-05 07:00:42 +0000

    while and do while loop

---
+++ 5_iterations/whileDOwhileloop.js
@@ -0,0 +1,23 @@
+let k=0;
+while (k <= 10 ) {
+    // console.log(`the value of k is : ${k}`);
+    k= k +3;
+}
+
+let fruits = ["apple", "banana", "cherry", "date"];
+let fr = 0
+while (fr < fruits.length) {
+    // console.log(`these are fruits : ${fruits[fr]}` );
+//console.log("these are fruits :", fruits[fr]);
+    
+    fr = fr + 1 ;
+}
+
+
+
+let score = 1
+do {
+ console.log("your score is :", score )
+ score ++
+  
+} while (score <= 11);

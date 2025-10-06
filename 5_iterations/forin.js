c94d7219b9d8e9ed4e00a4abd72b3cd911b95578
Kartik Rawat <kartikrwt2724@gmail.com>
2025-10-06 07:11:17 +0000

    for in loop

---
+++ 5_iterations/forin.js
@@ -0,0 +1,17 @@
+
+
+const myobj = {
+    car1 : "porche",
+    car2 : "mercedes",
+    car3 : "bentley",
+    car4 : "BMW",
+}
+for (const key in myobj) {
+   // console.log(key ," is " , myobj[key]);
+        
+}
+
+const lang = ["js", "c++", "node", "ruby", "c" ] 
+    for (const key in lang) {
+            console.log(lang[key]);       
+    }
